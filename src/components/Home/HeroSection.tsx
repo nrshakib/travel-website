467e918c7d8720b176dac0fa6522c120ddf4676c
nrshakib <nrshakib007@gmail.com>
2025-06-02 10:51:25 +0600

    Modified Hero Section

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -14,7 +14,7 @@ export default function HeroSection() {
         className="w-full h-full object-cover"
       />
       <div className="absolute z-50 w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-        <div className="flex flex-col items-center justify-center  w-full h-full">
+        <div className="flex flex-col items-center justify-center w-full h-full mt-20 sm:mt-0">
           <div>
             <h1 className="text-2xl md:text-3xl lg:text-4xl mb-4 md:mb-2 text-center text-white tracking-[0.7rem] font-bold uppercase">
               Plan & Enjoy The Trip
--- src/components/utilities/SearchBox.tsx
+++ src/components/utilities/SearchBox.tsx
@@ -1,52 +1,70 @@
 "use client";
 
 import {
+  Autocomplete,
   Button,
   FormControl,
   FormControlLabel,
   Radio,
   RadioGroup,
+  TextField,
 } from "@mui/material";
 import React, { useState } from "react";
 import { FaLocationArrow } from "react-icons/fa";
 import { FaMapLocation } from "react-icons/fa6";
 import { BsCalendar2DateFill } from "react-icons/bs";
+import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { Dayjs } from "dayjs";
+
+const locations = ["London", "Paris", "NY", "Barcelona", "Istanbul"];
 
 export default function SearchBox() {
   const [selectedTrip, setSelectedTrip] = useState<string>("one-way");
 
-  // Proper typing for the onChange handler
+  const [from, setFrom] = useState<string | null>(null);
+  const [to, setTo] = useState<string | null>(null);
+
+  const [journeyDate, setJourneyDate] = useState<Dayjs | null>(null);
+  const [returnDate, setReturnDate] = useState<Dayjs | null>(null);
+
   const handleTripSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setSelectedTrip(value);
-    console.log(value);
+    setSelectedTrip(event.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log("From:", from);
+    console.log("To:", to);
+    console.log("Trip Type:", selectedTrip);
+    console.log("Journey Date:", journeyDate?.format("YYYY-MM-DD"));
+    if (selectedTrip === "round-way") {
+      console.log("Return Date:", returnDate?.format("YYYY-MM-DD"));
+    }
   };
 
   return (
-    <div className="bg-white rounded-lg p-8 mt-4 sm:mt-12 w-[95%] sm:w-[80%] mx-auto">
-      <div className="mb-2">
+    // main div
+    <div className="relative bg-white rounded-lg p-4 sm:p-8 mt-4 sm:mt-12 w-[95%] sm:w-full mx-auto shadow-md">
+      {/* Trip Type */}
+      <div className="mb-6">
         <FormControl component="fieldset" fullWidth>
-          <RadioGroup
-            aria-labelledby="demo-row-radio-buttons-group-label"
-            name="row-radio-buttons-group"
-            value={selectedTrip}
-            onChange={handleTripSelect}
-            row
-            sx={{ display: "flex", flexDirection: "row" }}
-          >
+          <RadioGroup value={selectedTrip} onChange={handleTripSelect} row>
             <FormControlLabel
               value="one-way"
               control={
                 <Radio
                   sx={{
                     color: "#162456",
-                    "&.Mui-checked": {
-                      color: "#000",
-                    },
+                    "&.Mui-checked": { color: "#000" },
                   }}
                 />
               }
               label="One Way"
+              sx={{
+                "& .MuiFormControlLabel-label": {
+                  fontSize: { xs: "0.75rem", sm: "1rem" },
+                },
+              }}
             />
             <FormControlLabel
               value="round-way"
@@ -54,71 +72,112 @@ export default function SearchBox() {
                 <Radio
                   sx={{
                     color: "#162456",
-                    "&.Mui-checked": {
-                      color: "#000",
-                    },
+                    "&.Mui-checked": { color: "#000" },
                   }}
                 />
               }
               label="Round Way"
+              sx={{
+                "& .MuiFormControlLabel-label": {
+                  fontSize: { xs: "0.75rem", sm: "1rem" },
+                },
+              }}
             />
           </RadioGroup>
         </FormControl>
       </div>
 
-      <div
-        className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  ${
-          selectedTrip === "round-way" ? "xl:grid-cols-5" : "xl:grid-cols-4"
-        } items-center justify-center gap-8 w-full`}
-      >
-        <div className="col-span-1 flex items-center gap-2">
-          <FaMapLocation className="text-3xl" />
-          <div>
-            <p>From</p>
-            <p>From</p>
+      <div className="flex flex-col xl:flex-row lg:flex-nowrap gap-8 justify-between mb-6">
+        {/* From & To */}
+        <div className="flex flex-col lg:flex-row sm:flex-nowrap gap-6 xl:gap-8 flex-shrink-0 flex-grow-0 lg:flex-grow xl:basis-1/2 min-w-0">
+          {/* From */}
+          <div className="flex items-center gap-2 flex-grow min-w-[150px] max-w-[250px] min-h-[48px]">
+            <FaMapLocation className="sm:text-2xl text-[#162456]" />
+            <Autocomplete
+              options={locations}
+              value={from}
+              onChange={(_, newValue) => setFrom(newValue)}
+              sx={{ width: { xs: "100%", sm: "90%" }, flexGrow: 1 }}
+              freeSolo
+              renderInput={(params) => (
+                <TextField {...params} label="From" variant="standard" />
+              )}
+            />
+          </div>
+          {/* To */}
+          <div className="flex items-center gap-2 flex-grow  min-w-[150px] max-w-[250px] min-h-[48px]">
+            <FaLocationArrow className="sm:text-2xl text-[#162456]" />
+            <Autocomplete
+              options={locations}
+              value={to}
+              onChange={(_, newValue) => setTo(newValue)}
+              sx={{ width: { xs: "100%", sm: "90%" }, flexGrow: 1 }}
+              freeSolo
+              renderInput={(params) => (
+                <TextField {...params} label="To" variant="standard" />
+              )}
+            />
           </div>
         </div>
-        <div className="col-span-1 flex items-center gap-2 ">
-          <FaLocationArrow className="text-3xl" />
-          <div>
-            <p>To</p>
-            <p>To</p>
+
+        {/* Dates and Search */}
+        <div className="flex flex-col lg:flex-row sm:flex-nowrap items-center gap-6 sm:gap-8 flex-shrink-0 flex-grow-0 lg:flex-grow xl:basis-[40%] min-w-0 justify-center sm:justify-start">
+          {/* Journey Date */}
+          <div className="flex items-center gap-2 flex-grow min-w-[175px] max-w-[350px] min-h-[48px]">
+            <BsCalendar2DateFill className="sm:text-2xl text-[#162456]" />
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DesktopDatePicker
+                label="Journey Date"
+                value={journeyDate}
+                onChange={(newValue) => setJourneyDate(newValue)}
+                slotProps={{ textField: { size: "small", fullWidth: true } }}
+                sx={{ width: "100%" }}
+              />
+            </LocalizationProvider>
           </div>
-        </div>
-        <div
-          className={`${
-            selectedTrip === "round-way" ? "col-span-2" : "col-span-1"
-          } flex items-center gap-8`}
-        >
-          <div className="flex items-center gap-2">
-            <BsCalendar2DateFill className="text-3xl" />
-            <div>
-              <p>Journey Date</p>
-              <p>Pick A Date</p>
-            </div>
-          </div>
+
+          {/* Return Date */}
           {selectedTrip === "round-way" && (
-            <div className="flex items-center gap-2">
-              <FaMapLocation className="text-3xl" />
-              <div>
-                <p>Return Date</p>
-                <p>Pick A Date</p>
-              </div>
+            <div className="flex items-center gap-2 flex-grow min-w-[175px] max-w-[350px] min-h-[48px]">
+              <FaMapLocation className="sm:text-2xl text-[#162456]" />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DesktopDatePicker
+                  label="Return Date"
+                  value={returnDate}
+                  onChange={(newValue) => setReturnDate(newValue)}
+                  slotProps={{ textField: { size: "small", fullWidth: true } }}
+                  sx={{ width: "100%" }}
+                />
+              </LocalizationProvider>
             </div>
           )}
+
+          {/* Search Button */}
         </div>
-        <div>
-          <Button
-            sx={{
-              backgroundColor: "#162456",
-              "&:hover": {
-                backgroundColor: "#0e1832",
-              },
-            }}
-          >
-            Search
-          </Button>
-        </div>
+      </div>
+      <div className="absolute left-1/2 -bottom-5 transform -translate-x-1/2 flex justify-center flex-shrink-0 min-w-[120px] max-w-[140px]">
+        <Button
+          onClick={handleSearch}
+          variant="contained"
+          sx={{
+            backgroundColor: "#FDC700",
+
+            textTransform: "none",
+            fontSize: "18px",
+            color: "#162456",
+            fontWeight: {
+              sm: "400",
+              lg: "600",
+            },
+            py: 1,
+            "&:hover": { backgroundColor: "#162456", color: "white" },
+            width: "100%",
+            maxWidth: 140,
+            minWidth: 120,
+          }}
+        >
+          Search
+        </Button>
       </div>
     </div>
   );
